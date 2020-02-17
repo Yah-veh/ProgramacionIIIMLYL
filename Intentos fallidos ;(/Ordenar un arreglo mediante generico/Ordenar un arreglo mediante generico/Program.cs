dc2e357ce0040be3c4c281fd0a7fdc5d03a897de
Yah-veh <yersonmlisarazu@gmail.com>
2020-02-16 23:54:02 -0400

    Subiendo Tarea 2

---
+++ Intentos fallidos ;(/Ordenar un arreglo mediante generico/Ordenar un arreglo mediante generico/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordenar_un_arreglo_mediante_generico
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduzca la Cadena");
+            string a = Console.ReadLine();
+            char[] arreglo = a.ToCharArray();
+            Console.WriteLine("La Cadena Ordenada Contiene");
+            MuestraArreglo(arreglo);
+
+            Console.ReadKey();
+        }
+        // método genérico para mostrar un arreglo
+        private static void MuestraArreglo<T>(T[] arreglo)
+        {
+            Array.Sort(arreglo);
+            Array.Reverse(arreglo);
+            foreach (T elemento in arreglo)
+                Console.Write(elemento + " ");
+            Console.WriteLine("\n");
+        }
+    }
+}
+}
