7463e3ff36a2dd5cab0de6eabd72d77f002083a3
Yah-veh <yersonmlisarazu@gmail.com>
2020-03-09 10:43:41 +0100

    Act

--- Examen de Programacion/Ejercicio 3/Ejercicio 3/Program.cs
+++ Examen de Programacion/Ejercicio 3/Ejercicio 3/Program.cs
@@ -10,13 +10,21 @@ namespace Ejercicio_3
     {
         public delegate void Delegado(int n); 
         public event Delegado CuandoRecibaImaginario; 
-        public double Raices(int a, int b,int c)
+        public double RaicesPositiva(int a, int b,int c)
         {
             double raiz = Math.Sqrt(Math.Pow(b, 2) - 4 * a * c);
             int disparo = b * b - 4 * a * c;
-            double x1 = ((-b + raiz)) / (2 * a);
-            double x2 = ((-b - raiz)) / (2 * a);
+            double x1 = ((-b + raiz)) / (2 * a);        
             if ((disparo< 0) && (CuandoRecibaImaginario != null))  
+            { CuandoRecibaImaginario(disparo); }
+            return x1;
+        }
+        public double RaicesNegativa(int a, int b, int c)
+        {
+            double raiz = Math.Sqrt(Math.Pow(b, 2) - 4 * a * c);
+            int disparo = b * b - 4 * a * c;
+            double x2 = ((-b - raiz)) / (2 * a);
+            if ((disparo < 0) && (CuandoRecibaImaginario != null))
             { CuandoRecibaImaginario(disparo); }
             return x2;
         }
@@ -27,11 +35,17 @@ namespace Ejercicio_3
         {
             Operaciones o = new Operaciones();
             o.CuandoRecibaImaginario += RecibeImaginario;
-            double a, b, c;
-            a = double.Parse(Console.ReadLine());
-            b = double.Parse(Console.ReadLine());
-            c = double.Parse(Console.ReadLine());
-            double raices = o.Raices(a, b, c);
+            int a, b, c;
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+            c = int.Parse(Console.ReadLine());
+            double x1 = o.RaicesPositiva(a, b, c);
+            double x2 = o.RaicesNegativa(a, b, c);
+            Console.WriteLine("x1 = {0}", x1);
+            Console.WriteLine("x2 = {0}", x2);
+
+            Console.ReadKey();
+
 
         }
         static void RecibeImaginario(int n) // Manejador de Evento
