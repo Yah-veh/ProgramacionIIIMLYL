1b4f94dad28eb63beabd38ced63583e73b7c9266
Yah-veh <58156906+Yah-veh@users.noreply.github.com>
2020-05-17 19:07:52 -0400

    Actualizando Carpeta

---
+++ Unidad4/Parqueos/Parqueos/Puesto.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Parqueos
+{
+    class Puesto:IPuesto
+    {
+        protected int numero;
+        protected Auto auto;
+        protected DateTime inicio;
+        protected DateTime fin;
+
+        public Auto getAuto()
+        {
+            return auto;
+        }
+        public Puesto(int numero)
+        {
+            this.numero = numero;
+        }
+
+         public string ingresarAuto(Auto auto)
+        {
+            this.auto = auto;
+            inicio = DateTime.Now;
+            return inicio.ToLongDateString();
+        }
+
+        public double salirAuto()
+        {
+            this.auto = null;
+            fin = DateTime.Now;
+            TimeSpan total = fin - inicio;
+            return total.TotalHours;
+        }
+        public Boolean estaOcupado()
+        {
+            return (this.auto != null);
+        }
+    }
+}
