57c6f0e48156e308df2d4d8d4dc08f47321d9c3e
Yah-veh <yersonmlisarazu@gmail.com>
2020-06-10 11:04:18 -0400

    Socket

---
+++ Unidad6/Practica Sockets/Practica Sockets/Aritmetica.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica_Sockets
+{
+    class Aritmetica
+    {
+        private static Aritmetica instance;
+        
+        private Aritmetica()
+        {
+
+        }
+        public static Aritmetica getInstance()
+        {
+            if (instance == null)
+            {
+                instance = new Aritmetica();                
+            }           
+                return instance;
+        }
+
+
+        public double Sumar (int a, int b)
+        {
+            return a + b;
+
+        }
+        public double Restar(int a, int b)
+        {
+            return a - b;
+
+        }
+        public double Multiplicar(int a, int b)
+        {
+            return a * b;
+
+        }
+        public double Dividir(int a, int b)
+        {
+            return a / b;
+
+        }
+        public double Potencia(int a, int b)
+        {
+            return Math.Pow(a, b);
+        }
+        public double Raiz(int a, int b)
+        {
+            return Math.Pow(a,(1/b));
+
+        }
+    }
+}
---
+++ Unidad6/Practica Sockets/Practica Sockets/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica_Sockets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                int n;
+                Aritmetica o = Aritmetica.getInstance();
+
+                Console.Clear();  //Limpia la pantalla
+
+                //Entrada de Menu
+                Console.WriteLine("1. Sumar");
+                Console.WriteLine("2. Restar");
+                Console.WriteLine("3. Multiplicar");
+                Console.WriteLine("4. Dividir");
+                Console.WriteLine("5. Potencia");
+                Console.WriteLine("6. Raiz");
+
+                Console.WriteLine("");
+
+                Console.Write("Digite un valor entre 1-6: ");
+                n = int.Parse(Console.ReadLine());
+                Console.WriteLine("");
+
+                //Proceso
+                switch (n)
+                {
+                    case 1:
+                        Console.WriteLine("Valor de a");
+                        int a = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Valor de b");
+                        int b = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine();
+                       Console.WriteLine("Resultado "+o.Sumar(a,b));
+                        break;
+                    case 2:
+                        Console.WriteLine("Valor de a");
+                        a = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Valor de b");
+                        b = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine();
+                        Console.WriteLine("Resultado " + o.Restar(a, b));
+                        break;
+                    case 3:
+                        Console.WriteLine("Valor de a");
+                        a = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Valor de b");
+                        b = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine();
+                        Console.WriteLine("Resultado " + o.Multiplicar(a, b));
+                        break;
+                    case 4:
+                        Console.WriteLine("Valor de a");
+                        a = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Valor de b");
+                        b = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine();
+                        Console.WriteLine("Resultado " + o.Dividir(a, b));
+                        break;
+                    case 5:
+                        Console.WriteLine("Valor de a");
+                        a = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Valor de b");
+                        b = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine();
+                        Console.WriteLine("Resultado " + o.Potencia(a, b));
+                        break;
+                    case 6:
+                        Console.WriteLine("Valor de a");
+                        a = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Valor de b");
+                        b = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine();
+                        Console.WriteLine("Resultado " + o.Raiz(a, b));
+                        break;
+    
+                    default:
+
+                        Console.WriteLine("Introduzca un valor entre 1 y 6");
+                        break;
+                }
+                Console.WriteLine("");
+                Console.Write("Digite (Intro) para continuar, (Esc) para salir ...");
+
+            } while (Console.ReadKey().Key != ConsoleKey.Escape);
+            Console.ReadKey();
+
+
+        }
+    }
+}
